c889b310991468a7d646d233ca8aab2ac666ba3b
Dmitry Astapchik <astapchik.dm@gmail.com>
2024-05-27 17:55:43 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return Task.FromResult(_context.Orders.AsEnumerable()
+                                                  .Where(o => o.Quantity > 1)
+                                                  .MaxBy(o => o.CreatedAt));
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return Task.FromResult(_context.Orders.Where(o => o.User.Status == Enums.UserStatus.Active)
+                                                  .OrderBy(o => o.CreatedAt)
+                                                  .ToList());
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,29 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> GetUser()
+        {
+            return Task.FromResult(_context.Users.AsEnumerable()
+                                                 .MaxBy(u => u.Orders?.Where(o => o.CreatedAt.Year == 2003 && o.Status == Enums.OrderStatus.Delivered)
+                                                                      .Select(o => o.Price * o.Quantity)));
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return Task.FromResult(_context.Users.Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2010 && o.Status == Enums.OrderStatus.Paid))
+                                                 .ToList());
+        }
+    }
+}
